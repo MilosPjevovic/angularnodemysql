58f6b7cfcc8239e112fd53fb7a796a460ddf0ebe
Milos Pjevovic <dk.misa@hotmail.com>
2022-04-16 00:17:19 +0200

    added full REST CRUD method

--- backend/index.js
+++ backend/index.js
@@ -29,6 +29,143 @@ db.connect(err=>{
 })
 
 
+//get all data
+
+app.get('/user',(req,res)=>{
+
+    let qr = `select * from user`;
+
+    db.query(qr,(err,result)=>{
+        if(err){
+            console.log(err,'error')
+        }else if(result.length>0){
+            res.send({
+                message:'all user data',
+                data:result
+            })
+        }
+
+    })
+
+})
+
+
+//get single data 
+
+app.get('/user/:id',(req,res)=>{
+
+    let gID = req.params.id;
+
+    let qr = `select * from user where id = ${gID}`;
+
+    db.query(qr,(err,result)=>{
+
+        if(err){
+            console.log(err, 'error')
+        }else if(result.length>0){
+
+            res.send({
+                message:'get single data',
+                data:result
+            })
+        }else {
+            res.send({
+                message:'data not found'
+            })
+        }
+
+    })
+
+})
+
+//create data
+
+app.post('/user',(req,res)=>{
+    
+    console.log(req.body,'postdata');
+
+    let username = req.body.username;
+    let password = req.body.password;
+    let email = req.body.email;
+
+    let qr = `insert into user(username,password,email) 
+                values('${username}','${password}','${email}')`;
+
+                console.log(qr);
+
+        db.query(qr,(err,result)=>{
+
+            if(err){
+                console.log(err,'error')
+            }else{
+                res.send({
+                    message:'data inserted'
+                })
+            }
+
+        })        
+
+})
+
+
+//update data
+
+app.put('/user/:id',(req,res)=>{
+    
+    console.log(req.body,'updatedata');
+
+    let gID = req.params.id;
+    let username = req.body.username;
+    let password = req.body.password;
+    let email = req.body.email;
+
+    let qr = `update user set username = '${username}', password = '${password}', email = '${email}' where id=${gID}`;
+
+                console.log(qr);
+
+        db.query(qr,(err,result)=>{
+
+            if(err){
+                console.log(err,'error');
+            }else{
+                res.send({
+                    message:'data updated'
+                });
+            }
+
+        })        
+
+})
+
+
+//delete single data
+
+app.delete('/user/:id',(req,res)=>{
+    
+    console.log(req.body,'deletedata');
+
+    let gID = req.params.id;
+   
+    let qr = `delete from user where id=${gID}`;
+
+                console.log(qr);
+
+        db.query(qr,(err,result)=>{
+
+            if(err){
+                console.log(err,'error');
+            }else{
+                res.send({
+                    message:'data deleted'
+                });
+            }
+
+        })        
+
+})
+
+
+
 app.listen(3000,()=>{
     console.log('server runing..')
 })
