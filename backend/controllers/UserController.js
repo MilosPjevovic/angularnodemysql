1083ebb34005acd84624dcacbaaa9b57cffa7b93
Milos Pjevovic <dk.misa@hotmail.com>
2022-04-21 12:41:36 +0200

    added crud method for user

--- backend/controllers/UserController.js
+++ backend/controllers/UserController.js
@@ -36,6 +36,75 @@ module.exports = {
                 }
             })
         })
-     }
+     },
+
+
+
+     createUser: (req,res)=>{
+        const username = req.body.username;
+        const password = req.body.password;
+        const email = req.body.email; 
+        const qr = `insert into user(username,password,email) 
+        values('${username}','${password}','${email}')`;
+        
+         db.connect((err)=>{
+             if(err) throw (err);
+             db.query(qr,(err,result) => {
+              //   db.destroy();
+                 if(err){
+                     return err.message;
+                 }else{
+                     res.status(200).json(
+                        {message:'inserted data'}
+                     )
+                 }
+             })
+         })
+      },
+
+    
+      
+      updateUser: (req,res)=>{
+        const gID = req.params.id;
+        const username = req.body.username;
+        const password = req.body.password;
+        const email = req.body.email;
+        const qr = `update user set username = '${username}', password = '${password}', email = '${email}' where id=${gID}`;
+        
+         db.connect((err)=>{
+             if(err) throw (err);
+             db.query(qr,(err,result) => {
+              //   db.destroy();
+                 if(err){
+                     return err.message;
+                 }else{
+                     res.status(200).json(
+                        {message:'update data'}
+                     )
+                 }
+             })
+         })
+      },
+
+
+
+      deleteUser: (req,res)=>{
+        const gID = req.params.id;
+        const qr = `delete from user where id=${gID}`;
+        
+         db.connect((err)=>{
+             if(err) throw (err);
+             db.query(qr,(err,result) => {
+              //   db.destroy();
+                 if(err){
+                     return err.message;
+                 }else{
+                     res.status(200).json(
+                        {message:'deleted data'}
+                     )
+                 }
+             })
+         })
+      }
 }
 
--- backend/routes/User.js
+++ backend/routes/User.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const router = express.Router();
 
-const controler = require('../controllers/UserController')
+const userControler = require('../controllers/UserController')
 
-router.get('/',controler.get)
-      .get('/:id',controler.getById)
-
+router.get('/',userControler.get)
+      .get('/:id',userControler.getById)
+      .post('/',userControler.createUser)
+      .put('/:id',userControler.updateUser)
+      .delete('/:id',userControler.deleteUser)  
 
 
 module.exports = router;
